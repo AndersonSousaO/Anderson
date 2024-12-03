f30101a95cbd247fe6938e230ac797bbb5abc9e6
AndersonSousaO <andoliveira.mis@gmail.com>
2024-12-03 16:48:55 -0300

    c

---
+++ Linguagem C/soma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int x,y,soma;
+
+    printf("Digite o primeiro numero\n");
+    scanf("%d", &x);
+
+    printf("Digite o segundo numero\n");
+    scanf("%d", &y);
+
+    soma = x+y;
+
+    printf("O resultado e:%d",  x+y);
+}
