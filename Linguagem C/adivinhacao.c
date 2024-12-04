59503e606603a3eb4d9d4199bdd45e61b30de470
AndersonSousaO <andoliveira.mis@gmail.com>
2024-12-04 15:10:32 -0300

    test

--- Linguagem C/adivinhacao.c
+++ Linguagem C/adivinhacao.c
@@ -11,8 +11,40 @@ int main(){
 
     int chute;
 
-    printf("Qua e o seu chute?");
+    /*printf("Qua e o seu chute? ");
     scanf("%d", &chute);
-    printf("Seu chute foi %d", chute);
+    printf("Seu chute foi %d \n", chute);
+    */
+    int tentativa = 5;
+
+    while (tentativa != 0){
+        printf("\nResta %d numero de tentativa\n", tentativa);
+        printf("Qua e o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d \n", chute);
+        tentativa =tentativa -1;
+
+            if (chute == numerosecreto){
+                printf("Parabens! Voce acertou\n");
+                printf("Jogou bem!\n");
+                tentativa =0;
+            }
+            else
+            {
+            if (chute > numerosecreto){
+                printf("Seu chute foi maior que o numero Secreto!\n");
+            }
+            
+            if (chute < numerosecreto){
+                printf("Seu chute foi menor que o numero Secreto!\n");
+            }
+            
+            
+            }
+    
+   
+
+    }
+    
 
 }
